484a015730b66ca434db8d9ef123b7a4c6f296d3
Cathaoir <3510166+JimmyCastiel@users.noreply.github.com>
2025-04-04 19:51:27 +0900

    Add: oidc dep

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #[macro_use] extern crate rocket;
-use rocket::State;
-use rocket::http::Status;
-use rocket::serde::{
-    Deserialize,
-    Serialize,
-    json::{
-        to_string,
-        Json
+use rocket::{
+    State,
+    http::Status,
+    serde::{
+        Deserialize,
+        Serialize,
+        json::{
+            to_string,
+            Json
+        }
     }
 };
 use uuid7::uuid7;
@@ -14,23 +16,45 @@ use uuid7::uuid7;
 use env;
 
 use std::collections::LinkedList;
+use std::str::FromStr;
 use std::time::Duration;
 
-use rdkafka::config::ClientConfig;
-use rdkafka::message::{
-    Header,
-    OwnedHeaders
+use openidconnect::{
+    core::{
+        CoreClient,
+        CoreIdToken,
+        CoreIdTokenVerifier,
+        CoreJsonWebKey,
+        CoreJsonWebKeySet,
+        CoreJsonWebKeyUse,
+        CoreProviderMetadata
+    },
+    ClientId,
+    ClientSecret,
+    IssuerUrl,
+    JsonWebKey,
+    JsonWebKeyUse,
+    reqwest,
+    reqwest::ClientBuilder
 };
-use rdkafka::producer::{
-    FutureProducer,
-    FutureRecord
+
+use rdkafka::{
+    config::ClientConfig,
+    message::{
+        Header,
+        OwnedHeaders
+    },
+    producer::{
+        FutureProducer,
+        FutureRecord
+    }
 };
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
 struct Message {
-    oa: String,
-    da: String,
+    source: String,
+    destination: String,
     body: String
 }
 
@@ -62,20 +86,60 @@ async fn send_message(message: Json<Message>, state: &State<FutureProducer>) -> 
 }
 
 #[get("/<message_id>")]
-fn get_message(message_id: &str) -> Json<Message> {
+async fn get_message(message_id: &str) -> Json<Message> {
     info!("message with id {} was requested", message_id);
     // TODO implement authentication
-    Json(Message{oa: "".to_string(), da: "".to_string(), body: "".to_string()})
+    Json(Message{source: "".to_string(), destination: "".to_string(), body: "".to_string()})
 }
 
 #[get("/")]
-fn list_messages() -> Json<Messages> {
+async fn list_messages() -> Json<Messages> {
     // TODO implement authentication
     Json(LinkedList::new())
 }
 
+async fn init_oidc() {
+    let http_client = ClientBuilder::new()
+        // Following redirects opens the client up to SSRF vulnerabilities.
+        .redirect(reqwest::redirect::Policy::none())
+        .build()
+        .expect("Client should build");
+    let client_id: ClientId = ClientId::new("client_id".to_string());
+    let issuer_url: IssuerUrl = IssuerUrl::new("https://accounts.example.com".to_string()).unwrap(); 
+    let id_token: CoreIdToken = CoreIdToken::from_str("YOUR_ID_TOKEN").unwrap();
+    
+    // Use OpenID Connect Discovery to fetch the provider metadata.
+    let provider_metadata = CoreProviderMetadata::discover_async(
+        issuer_url.clone(),
+        &http_client,
+    )
+    .await
+    .unwrap();
+
+    let client = CoreClient::from_provider_metadata(
+        provider_metadata.clone(),
+        client_id.clone(),
+        Some(ClientSecret::new("client_secret".to_string())),
+    );
+
+    let jwks_uri = provider_metadata.jwks_uri();
+
+    let jwks = CoreJsonWebKeySet::fetch_async(&jwks_uri, &http_client).await.unwrap();
+
+    let verifier: CoreIdTokenVerifier = client.id_token_verifier();
+
+    let signing_key: &CoreJsonWebKey = id_token.signing_key(&verifier).unwrap();
+
+    let algo = signing_key.signing_alg();
+
+    //signing_key.verify_signature(signing_key.signing_alg().unwrap, b"", b"");
+
+    // Verify the token
+    //let claims = id_token.claims(&verifier, jwks);
+}
+
 #[launch]
-fn rocket() -> _ {
+async fn rocket() -> _ {
     let producer: FutureProducer = ClientConfig::new()
         .set("bootstrap.servers", env::var("KAFKA_BROKERS").unwrap().parse::<String>().unwrap())
         .set("message.timeout.ms", "5000")
