4c9c1b4e575810811b7e9c13b53efcd3f219de78
Cathaoir <3510166+JimmyCastiel@users.noreply.github.com>
2025-04-10 21:04:16 +0900

    Fix: streamline kafka.rs

--- src/kafka.rs
+++ src/kafka.rs
@@ -2,10 +2,7 @@ use thiserror::Error;
 
 use rdkafka::{config::ClientConfig, producer::{FutureProducer, FutureRecord}};
 
-use std::{
-    env,
-    time::Duration,
-};
+use std::{env, time::Duration};
 
 #[derive(Error, Debug)]
 pub(crate) enum KafkaError {
@@ -22,35 +19,24 @@ pub(crate) struct KafkaClient {
 
 impl KafkaClient {
     pub async fn send_message(&self, key: &str, message: &str) -> Result<(), KafkaError> {
-        let record = FutureRecord::to(&self.topic)
-            .key(key)
-            .payload(message);
-
-        let response = self.producer.send(record, Duration::from_secs(0)).await;
-
-        match response {
-            Ok(_) => Ok(()),
-            _ => Err(KafkaError::MessageDeliveryError),
-        }
+        self.producer
+            .send(
+                FutureRecord::to(&self.topic).key(key).payload(message),
+                Duration::from_secs(0),
+            )
+            .await
+            .map(|_| ())
+            .map_err(|_| KafkaError::MessageDeliveryError)
     }
 
     pub fn new() -> Result<Self, KafkaError> {
         let producer = ClientConfig::new()
-            .set(
-                "bootstrap.servers",
-                env::var("KAFKA_BROKERS")
-                    .expect("KAFKA_BROKERS environment variable is not set")
-                    .parse::<String>()
-                    .unwrap(),
-            )
+            .set("bootstrap.servers", &env::var("KAFKA_BROKERS").expect("KAFKA_BROKERS not set"))
             .set("message.timeout.ms", "5000")
             .create()
             .map_err(|_| KafkaError::ProducerCreationError)?;
 
-        let topic = env::var("KAFKA_TOPIC")
-            .expect("KAFKA_TOPIC environment variable is not set")
-            .parse::<String>()
-            .unwrap();
+        let topic = env::var("KAFKA_TOPIC").expect("KAFKA_TOPIC not set");
 
         Ok(KafkaClient { producer, topic })
     }
